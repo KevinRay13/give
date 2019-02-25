03b20a639466a0388884ec659d199d2f682ef7f5
Kevin Edmondson <kevinedmondson@Kevins-MacBook-Pro.local>
2019-02-25 09:05:17 -0600

    home page adds

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./Home.module.css";
+import "./Home.css";
 import Carousel from "./Carousel";
 import { connect } from "react-redux";
 import { getUser } from "../../ducks/reducer";
@@ -21,7 +21,40 @@ class Home extends Component {
   render() {
     return (
       <div className="home-landing">
+        <div className="banner">
+          <img
+            src="https://cdn.shopify.com/s/files/1/1863/0841/files/rsz__mg_1198-compressor_1024x1024.jpg?v=1501186091"
+            alt=""
+            className="banner"
+          />
+        </div>
         <Carousel />
+        <div className="aboutcont">
+          <div>
+            <img
+              src="https://missmuslim.nyc/store/wp-content/uploads/2018/12/hat-2.jpg"
+              alt="hats"
+              className="manyhats"
+            />
+          </div>
+          <div className="givemessage">
+            <div className="message">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0445/0437/t/195/assets/50icon.png?9019808226547638753"
+                alt=""
+                className="messageimg"
+              />
+              <h2>50% of Profit Given</h2>
+              <h5>
+                Fifty percent (50%) of net profit from the sale of all (give)
+                products is given to the (give) Fund to support our nonprofit
+                partners in the fight against pediatric cancer, create
+                therapeutic experiences and fund charitable programming
+                initiatives for children and families battling cancer.
+              </h5>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
