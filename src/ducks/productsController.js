e6d8d0047005862a0bddf4a5b7c72397efebc00e
Kevin Edmondson <kevinraye13@gmail.com>
2019-03-04 14:07:44 -0600

    remove local host

--- src/ducks/productsController.js
+++ src/ducks/productsController.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const getAllProducts = function() {
-  return axios
-    .get("http://localhost:5050/inventory/products")
-    .then(response => response.data);
+  return axios.get("/inventory/products").then(response => response.data);
 };
+//http://localhost:5050
